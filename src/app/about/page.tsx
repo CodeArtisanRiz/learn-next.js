71702e6391aaa115de89fa38eec97c39472c3169
CodeArtisanRiz <riz1.xtreme@gmail.com>
2025-01-06 12:17:02 +0530

    useRouter (page switch on btn click)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+import { useRouter } from "next/navigation";
 export default function AboutPage() {
-  return <h1>About Page</h1>;
+  const router = useRouter();
+  return (
+    <div className="p-4">
+      <h1>About Page</h1>
+      <div className="content-center text-center">
+      <button onClick={() => router.push("/")} className="bg-blue-500 text-white p-2 rounded-md">Go to Home</button>
+      </div>
+      
+    </div>
+  );
 }
