c7753a12d9df2329563ba40b26fb2966f5a197f1
CodeArtisanRiz <riz1.xtreme@gmail.com>
2025-01-06 17:40:33 +0530

    Data fetching in server-components

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,22 @@
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+};
+
+export default async function UsersServer() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users: User[] = await res.json();
+    return (
+        <ul className="space-y-4 p-4 container mx-auto">
+            {users.map((user: User) => (
+                <li key={user.id} className="bg-white border p-4 rounded-md shadow-md">
+                    <h2 className="text-lg font-semibold text-gray-800">{user.name}</h2>
+                    <p className="text-gray-700">{user.email}</p>
+                    <p className="text-gray-700">{user.phone}</p>
+                </li>
+            ))}
+        </ul>
+    );
+}
