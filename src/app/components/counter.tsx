01a37562cbef7693ff5d84949d9355670d83c247
CodeArtisanRiz <riz1.xtreme@gmail.com>
2025-01-19 22:50:34 +0530

    Show ui elements based on auth status

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,9 +1,19 @@
 "use client"; // This file is client-side only
-import { useState } from "react";
+import { use, useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 export const Counter = () => {
+
+    // const { isLoaded, userId, isSignedIn, getToken } = useAuth(); // or
+    const { isLoaded, isSignedIn, userId } = useUser();
+
+    
+
     console.log("Counter Component");
     const [count, setCount] = useState(0);
+    if(
+        !isLoaded || !isSignedIn // or  !userId
+        ) return null;
     return (
         <button onClick={() => setCount(count + 1)}>
             Clicked {count} times
